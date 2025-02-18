963dbf45a20ed7ca26215938836eae8c7fc2c48f
tettekete <6535634+tettekete@users.noreply.github.com>
2025-02-18 21:50:31 +0900

    feat: add QuickPick preview for "Go to References" results
    - Implemented a QuickPick preview displaying reference locations when "Go
      to References" is selected.

--- src/open-definition.ts
+++ src/open-definition.ts
@@ -1,13 +1,17 @@
 import * as vscode from 'vscode';
 
 import { getViewColumnForOpen } from './lib/decide-view-column';
-import { getActiveTextEditorForTabGroup } from './lib/utils';
+import {
+	getActiveTextEditorForTabGroup,
+	findWorkspaceFolder
+} from './lib/utils';
 import type { ValidProviderCommands } from './constants';
 import { HightLightBox } from './lib/hight-light-box';
-import { sleep } from './lib/sleep';
 
 import type { LocationOrLocationLink } from './lib/location-or-location-link';
 import { getUriRangeFromLocationOrLocationLink } from './lib/location-or-location-link';
+import { getLocationTextEntries } from './lib/get-location-text-entries';
+import path from 'node:path';
 
 
 export async function openDefinitionInSidePane( providerCommand: ValidProviderCommands ):Promise<boolean>
@@ -26,15 +30,18 @@ export async function openDefinitionInSidePane( providerCommand: ValidProviderCo
 		return false;
 	}
 
-	{
-	}
-	{
-	}
-
 	const targetViewColumn = getViewColumnForOpen();
+	let openedViewColumn = targetViewColumn;
+	if( targetViewColumn === vscode.ViewColumn.Beside )
+	{
+		openedViewColumn = ((vscode.window.tabGroups.activeTabGroup.viewColumn - 1) % 8) + 1;
+	}
 
-	console.debug(`targetViewColumn: ${targetViewColumn}`);
-	console.debug(`theUri: ${theUri}`);
+	if( locations.length > 1 )
+	{
+		navigateWithQuickPick({ locations ,viewColumn: openedViewColumn });
+		return true;
+	}
 	
 	const r = getUriRangeFromLocationOrLocationLink( locations[0] );
 	let theUri: vscode.Uri = r.uri;
@@ -50,14 +57,6 @@ export async function openDefinitionInSidePane( providerCommand: ValidProviderCo
 
 
 	// Scroll to definition pos
-	let openedViewColumn = targetViewColumn;
-	if( targetViewColumn === vscode.ViewColumn.Beside )
-	{
-		openedViewColumn = ((vscode.window.tabGroups.activeTabGroup.viewColumn - 1) % 8) + 1;
-
-		console.debug( `openedViewColumn: ${openedViewColumn}` );
-	}
-
 	const defineEditor = getActiveTextEditorForTabGroup( openedViewColumn );
 	if( defineEditor )
 	{
@@ -76,11 +75,101 @@ export async function openDefinitionInSidePane( providerCommand: ValidProviderCo
 		// Show HightLightBox
 		HightLightBox.show({
 			editor: defineEditor,
-			range: theRange,
-			// timeOut: 0.4,
-			// opacityDelta: 0.1
+			range: theRange
 		});
 	}
 
 	return true;
+}
+
+interface RefQuicPickItem extends vscode.QuickPickItem
+{
+	uri: vscode.Uri
+	range: vscode.Range
+}
+
+async function navigateWithQuickPick(
+	{
+		locations,
+		viewColumn
+	}:
+	{
+		locations: LocationOrLocationLink[];
+		viewColumn: vscode.ViewColumn;
+	}
+)
+{
+	const locationTextEntries = await getLocationTextEntries( locations );
+
+	const qpItems:RefQuicPickItem[] = [];
+
+	for( const entry of locationTextEntries )
+	{
+		const workspaceFolder = findWorkspaceFolder( entry.uri.fsPath );
+		let relPath:string;
+		if( workspaceFolder )
+		{
+			relPath = path.relative( workspaceFolder , entry.uri.fsPath );
+		}
+		else
+		{
+			relPath = path.basename( entry.uri.fsPath );
+		}
+
+		const label = relPath;
+
+		const description = `Line: ${entry.range.start.line}`;
+
+		qpItems.push(
+			{
+				label,
+				description,
+				uri: entry.uri,
+				range: entry.range
+			}
+		);
+	}
+
+	const quickPick = vscode.window.createQuickPick<RefQuicPickItem>();
+	quickPick.items = qpItems;
+
+	// define preview callback
+	quickPick.onDidChangeActive((selectedItems) =>
+		{
+			const selection = selectedItems[0];
+			if( selection )
+			{
+				previewLocation( viewColumn , selection );
+			}
+		}
+	);
+
+	// define callback on decision by user
+	quickPick.onDidAccept(()=>
+		{
+			quickPick.hide();
+		}
+	);
+
+	// define callback in canceled by user
+	quickPick.onDidHide(() =>
+		{
+			quickPick.dispose();
+		}
+	);
+
+	// show QuickPick
+	quickPick.show();
+}
+
+
+function previewLocation( viewColumn:vscode.ViewColumn , refQPItem: RefQuicPickItem )
+{
+	vscode.window.showTextDocument( refQPItem.uri ,
+	{
+		viewColumn,
+		selection: refQPItem.range,
+		preview: true,
+		preserveFocus: true
+	});
 }
