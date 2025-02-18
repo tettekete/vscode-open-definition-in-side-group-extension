85f9e349d54ec0249666c4ac3c6fa86742f09282
tettekete <6535634+tettekete@users.noreply.github.com>
2025-02-18 21:46:46 +0900

    build: add findWorkspaceFolder() to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -76,3 +76,29 @@ function findTextEditorWithViewColumnAndPathString(
 
 	return foundEditor;
 }
+
+
+/**
+ * Find the workspace folder that contains the given file path.
+ * @param filePath The file path to check.
+ * @returns The root path of the workspace folder, or `undefined` if not found.
+ */
+export function findWorkspaceFolder(filePath: string): string | undefined
+{
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+
+    if ( ! workspaceFolders )
+	{
+        return undefined;
+    }
+
+	for (const folder of workspaceFolders)
+	{
+		if (filePath.startsWith(folder.uri.fsPath))
+		{
+			return folder.uri.fsPath;
+		}
+	}
+
+    return undefined;
+}
